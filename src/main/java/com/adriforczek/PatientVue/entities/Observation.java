7e3d3d740b09ece6ff03e2f72568e39bf4a59d37
Adri Forczek <forczekadri@gmail.com>
2020-02-02 23:20:18 +0000

    feature: setup observation service to handle business logic

--- src/main/java/com/adriforczek/PatientVue/entities/Observation.java
+++ src/main/java/com/adriforczek/PatientVue/entities/Observation.java
@@ -2,4 +2,6 @@ package com.adriforczek.PatientVue.entities;
 
 public class Observation {
 
+    // Jackson: deserialize JSON string into Java objects
+
 }
---
+++ src/main/java/com/adriforczek/PatientVue/services/ObservationService.java
@@ -0,0 +1,14 @@
+package com.adriforczek.PatientVue.services;
+
+import com.adriforczek.PatientVue.entities.Observation;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ObservationService {
+
+    public List<Observation> getAllObservations(){
+        return null;
+    }
+}
