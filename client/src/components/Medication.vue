b22921c7484e65b754d1c836c178e0c51dabc7ba
Adri Forczek <forczekadri@gmail.com>
2020-02-12 23:48:58 +0000

    feature: medications displayed in columns

--- client/src/components/Medication.vue
+++ client/src/components/Medication.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="tile">
-        {{ formatDate(this.medication.startDate) }}
-        {{ this.medication.name }}
-        {{ this.medication.dose }}
+    <div class="flex">
+        <div class="column">
+            {{ formatDate(this.medication.startDate) }}
+            <br/>
+            {{ this.medication.name }}
+        </div>
+        <div class="column">
+            {{ this.medication.dose }}
+        </div>
     </div>
 </template>
 
@@ -18,3 +23,13 @@
         }
     }
 </script>
+
+<style>
+
+    .flex {
+        font-size: small;
+        display: flex;
+        text-align: left;
+        justify-content: space-between;
+    }
+</style>
